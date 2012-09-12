a8f65f3119565dafd1462b71dbd9a110fb3e7521
thelastpenguin212 <thelastpenguin212@gmail.com>
2012-09-12 07:45:11 +0000

    Fixed an efficiency issue and made code simpler.

--- lua/PAdmin/plugins/cmd_cexec_sh.lua
+++ lua/PAdmin/plugins/cmd_cexec_sh.lua
@@ -10,17 +10,11 @@ tbl.permdefault = true
 
 tbl.run = function( ply, args )
 	local res = PAdmin:FindPlayersByName( args[1] )
+	table.remove( args, 1 )
+	command = table.concat( args, " " )
+			
 	for k,v in pairs( res ) do
 		if v:IsValid() then
-			local command = ""
-			for i,j in pairs( args ) do
-				if i > 1 then
-					command = command..j.." "
-				end
-			end
-			command = string.TrimRight( command )
-			command = command.."\n"
-			print("The command was: \""..command.."\"")
 			v:ConCommand( command )
 		end
 	end
