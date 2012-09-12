3ea9b52bd51a2befbd3c65bdcbfb5e8c7ec98d13
thelastpenguin212 <thelastpenguin212@gmail.com>
2012-09-12 07:54:16 +0000

    did more optimization and and general structure improvements.

--- lua/PAdmin/plugins/cmd_cexec_sh.lua
+++ lua/PAdmin/plugins/cmd_cexec_sh.lua
@@ -11,12 +11,10 @@ tbl.permdefault = true
 tbl.run = function( ply, args )
 	local res = PAdmin:FindPlayersByName( args[1] )
 	table.remove( args, 1 )
-	command = table.concat( args, " " )
+	local command = table.concat( args, " " )
 			
 	for k,v in pairs( res ) do
-		if v:IsValid() then
-			v:ConCommand( command )
-		end
+		v:ConCommand( command )
 	end
 end
 
