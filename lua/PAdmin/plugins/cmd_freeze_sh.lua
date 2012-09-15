51cbc3494dba15a42c74cc9f4a9869d1c405f481
tynerk92 <tynerk92@gmail.com>
2012-09-15 04:09:36 +0000

    Added random crap to cmd_freeze_sh.lua

--- lua/PAdmin/plugins/cmd_freeze_sh.lua
+++ lua/PAdmin/plugins/cmd_freeze_sh.lua
@@ -1,5 +1,16 @@
 local freeze = {}
 local unfreeze = {}
+local soundlist = {"physics/glass/glass_bottle_break1.wav",
+"physics/glass/glass_bottle_break2.wav",
+"physics/glass/glass_cup_break1.wav",
+"physics/glass/glass_cup_break2.wav",
+"physics/glass/glass_impact_bullet1.wav",
+"physics/glass/glass_impact_bullet2.wav",
+"physics/glass/glass_impact_bullet3.wav",
+"physics/glass/glass_largesheet_break1.wav",
+"physics/glass/glass_pottery_break3.wav",
+"physics/glass/glass_pottery_break4.wav"
+}
 
 freeze.format = {
 	{PAdmin.types.PLY, "target<ply>"}
@@ -16,7 +27,11 @@ unfreeze.permdefault = true
 
 freeze.run = function( ply, args )
 	local res = PAdmin:FindPlayersByName( args[1] )
+	local rand = math.random( 1, #soundlist )
+	print(rand)
+	print(soundlist[rand])
 	for k, v in pairs( res ) do
+		v:EmitSound( soundlist[rand], 100, 100 )
 		v:Freeze( true )
 		v:SetColor( Color(0, 100, 255) )
 	end
