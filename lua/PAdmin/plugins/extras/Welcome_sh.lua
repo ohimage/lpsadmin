8e3da85f36be06db3d530a367a159a3886fb681f
thelastpenguin212 <thelastpenguin212@gmail.com>
2012-09-22 10:26:58 +0000

    Changes to the countries decoder for Welcome_sh.lua plugin.

--- lua/PAdmin/plugins/extras/Welcome_sh.lua
+++ lua/PAdmin/plugins/extras/Welcome_sh.lua
@@ -8,17 +8,21 @@ elseif(SERVER)then
 		["LT"]="Lithuania",
 		["RU"]="Russia",
 		["AU"]="Australia",
-		["GB"]="England"
+		["GB"]="England",
+		["FL"]="Finland",
+		["KR"]="Korea",
+		["NZ"]="New Zeland",
 	}
 	local tbl = {}
 	concommand.Add("PA_Welcome",function( ply, cmd, args )
 		if( not args[1] )then return end
 		if( not table.HasValue( tbl, ply:UserID() ))then
 			table.insert(tbl, ply:UserID())
+			local country = args[1]
 			if( countries[ args[1] ] )then
-				args[1] = countries[ args[1] ]
+				country = countries[ args[1] ]
 			end
-			PAdmin:Notify( player.GetAll(), PAdmin.colors.neutral, "Welcome player ", ply, " from ", PAdmin.colors.purple, args[1] )
+			PAdmin:Notify( player.GetAll(), PAdmin.colors.neutral, "Welcome player ", ply, " from ", PAdmin.colors.purple, country )
 		end
 	end)
 end
