ce5d0dc47bf30dca5499957f8ffc20b0fe519a46
thelastpenguin212@gmail.com <thelastpenguin212@gmail.com@959303fb-05ac-5f0f-a67a-c91fabef4ca7>
2012-10-12 07:56:37 +0000

    Fixed error spam on noclip plugin.

--- trunk/lua/PAdmin/plugins/default/noclip_sh.lua
+++ trunk/lua/PAdmin/plugins/default/noclip_sh.lua
@@ -5,10 +5,12 @@ tbl.format = {}
 tbl.perm = "PAdmin.noclip"
 
 -- note. This should be reformated as a plugin that can be loaded and unloaded.
+local lastWarned = 0
 hook.Add("PlayerNoClip","PAdmin.CanNoclip",function( ply )
 	if( ply:GetMoveType() == MOVETYPE_NOCLIP )then return true end -- players can always un noclip.
-	if(CLIENT and not ply:HasPermission( "PAdmin.noclip" ))then
+	if(CLIENT and not ply:HasPermission( "PAdmin.noclip" ) and RealTime() - lastWarned > 5)then
 		chat.AddText(PAdmin.colors.error, "You dont have permission 'PAdmin.noclip'")
+		lastWarned = RealTime()
 	end
 	return ply:HasPermission( "PAdmin.noclip" )
 end)
